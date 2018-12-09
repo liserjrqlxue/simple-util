d7a099d36636eea100fd2ba89c561061a7ce3e9e
Wang Yaoshen <wangyaoshen@bgi.com>
2018-12-09 15:17:05 +0800

    add func load txt to map[string]map[string]

--- txt.go
+++ txt.go
@@ -64,3 +64,31 @@ func File2MapArray(path, sep string) []map[string]string {
 	CheckErr(scanner.Err())
 	return mapArray
 }
+
+// reads file to map[string]map[string]string
+func File2MapMap(path, key, sep string) map[string]map[string]string {
+	file, err := os.Open(path)
+	CheckErr(err)
+	defer DeferClose(file)
+
+	var db = make(map[string]map[string]string)
+	var title []string
+	scanner := bufio.NewScanner(file)
+	var i = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		array := strings.Split(line, sep)
+		if i == 0 {
+			title = array
+		} else {
+			var dataHash = make(map[string]string)
+			for j, k := range array {
+				dataHash[title[j]] = k
+			}
+			db[dataHash[key]] = dataHash
+		}
+		i++
+	}
+	CheckErr(scanner.Err())
+	return db
+}
