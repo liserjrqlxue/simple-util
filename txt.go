3e51d27031fc15d52bd465fe0450f34fa641a3cf
Wang Yaoshen <wangyaoshen@bgi.com>
2018-12-11 22:55:33 +0800

    add struct MapDb as map do not key keys order

--- txt.go
+++ txt.go
@@ -6,6 +6,11 @@ import (
 	"strings"
 )
 
+type MapDb struct {
+	Title []string
+	Data  []map[string]string
+}
+
 // reads file to []string
 func File2Array(path string) []string {
 	file, err := os.Open(path)
@@ -63,6 +68,37 @@ func File2MapArray(path, sep string) []map[string]string {
 	}
 	CheckErr(scanner.Err())
 	return mapArray
+}
+
+// reads file to []map[string]string
+func File2MapDb(path, sep string) MapDb {
+	file, err := os.Open(path)
+	CheckErr(err)
+	defer DeferClose(file)
+
+	var mapArray []map[string]string
+	var title []string
+	scanner := bufio.NewScanner(file)
+	var i = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		array := strings.Split(line, sep)
+		if i == 0 {
+			title = array
+		} else {
+			var dataHash = make(map[string]string)
+			for j, k := range array {
+				dataHash[title[j]] = k
+			}
+			mapArray = append(mapArray, dataHash)
+		}
+		i++
+	}
+	CheckErr(scanner.Err())
+	var mapDb MapDb
+	mapDb.Title = title
+	mapDb.Data = mapArray
+	return mapDb
 }
 
 // reads file to map[string]map[string]string
