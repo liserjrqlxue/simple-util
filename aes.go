83020ad4ab0ed0cd76b92737535e77de0fff958e
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-02-21 17:47:27 +0800

    add Encode2file

---
+++ aes.go
@@ -0,0 +1,22 @@
+package simple_util
+
+import (
+	"fmt"
+	"github.com/liserjrqlxue/crypto/aes"
+	"os"
+)
+
+func Encode2file(fileName string, data, codeKey []byte) {
+	file, err := os.Create(fileName)
+	CheckErr(err)
+	c := Encode2File(file, data, codeKey)
+	fmt.Printf("write %d byte to %s\n", c, fileName)
+}
+
+func Encode2File(file *os.File, data, codeKey []byte) int {
+	d, err := AES.Encode(data, codeKey)
+	CheckErr(err)
+	c, err := file.Write(d)
+	CheckErr(err)
+	return c
+}
