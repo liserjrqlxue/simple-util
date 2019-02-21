319462298c0e91e900b2634e56cba11f4a8f444c
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-02-21 17:23:10 +0800

    add Json2file

---
+++ json.go
@@ -0,0 +1,22 @@
+package simple_util
+
+import (
+	"fmt"
+	"os"
+)
+
+func Json2file(json []byte, filenName string) error {
+	file, err := os.Create(filenName)
+	if err != nil {
+		return err
+	}
+	DeferClose(file)
+
+	c, err := file.Write(json)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("write %d byte to %s\n", c, filenName)
+
+	return nil
+}
