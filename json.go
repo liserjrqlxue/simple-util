913c792bb4940ff1d699e196d5c16dd0cc8389a6
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-03-07 15:11:01 +0800

    add Json2Map

--- json.go
+++ json.go
@@ -30,9 +30,33 @@ func Json2MapMap(jsonBlob []byte) map[string]map[string]string {
 	return data
 }
 
+func Json2Map(jsonBlob []byte) map[string]string {
+	var data = make(map[string]string)
+	err := json.Unmarshal(jsonBlob, &data)
+	CheckErr(err)
+	return data
+}
+
 func JsonFile2MapMap(fileName string) map[string]map[string]string {
 	b, err := ioutil.ReadFile(fileName)
 	CheckErr(err)
 	fmt.Printf("load %d byte from %s\n", len(b), fileName)
 	return Json2MapMap(b)
+}
+
+func JsonFile2Map(fileName string) map[string]string {
+	b, err := ioutil.ReadFile(fileName)
+	CheckErr(err)
+	fmt.Printf("load %d byte from %s\n", len(b), fileName)
+	return Json2Map(b)
+}
+
+func JsonFile2Interface(fileName string) interface{} {
+	b, err := ioutil.ReadFile(fileName)
+	CheckErr(err)
+	fmt.Printf("load %d byte from %s\n", len(b), fileName)
+	var data interface{}
+	err = json.Unmarshal(b, &data)
+	CheckErr(err)
+	return data
 }
