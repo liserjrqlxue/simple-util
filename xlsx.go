e071d0df02c075e56e20827f0c66d1857d8d8a61
Wang Yaoshen <wangyaoshen@bgi.com>
2019-03-25 13:39:45 +0800

    excelFile update api

--- xlsx.go
+++ xlsx.go
@@ -5,7 +5,8 @@ import "github.com/360EntSecGroup-Skylar/excelize"
 func Sheet2MapArray(excelFile, sheetName string) ([]string, []map[string]string) {
 	xlsxFh, err := excelize.OpenFile(excelFile)
 	CheckErr(err)
-	rows := xlsxFh.GetRows(sheetName)
+	rows, err := xlsxFh.GetRows(sheetName)
+	CheckErr(err)
 
 	return Slice2MapArray(rows)
 }
@@ -13,7 +14,8 @@ func Sheet2MapArray(excelFile, sheetName string) ([]string, []map[string]string)
 func Sheet2MapMap(excelFile, sheetName, key string) ([]string, map[string]map[string]string) {
 	xlsxFh, err := excelize.OpenFile(excelFile)
 	CheckErr(err)
-	rows := xlsxFh.GetRows(sheetName)
+	rows, err := xlsxFh.GetRows(sheetName)
+	CheckErr(err)
 
 	return Slice2MapMap(rows, key)
 }
@@ -22,7 +24,8 @@ func Sheet2MapMapMerge(excelFile, sheetName, key, sep string) ([]string, map[str
 
 	xlsxFh, err := excelize.OpenFile(excelFile)
 	CheckErr(err)
-	rows := xlsxFh.GetRows(sheetName)
+	rows, err := xlsxFh.GetRows(sheetName)
+	CheckErr(err)
 
 	return Slice2MapMapMerge(rows, key, sep)
 }
