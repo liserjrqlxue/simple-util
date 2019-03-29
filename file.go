21c61cf52dd4cc028f9c07a5dbbb2c217f9420e1
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-03-29 14:51:45 +0800

    add FileExists check

---
+++ file.go
@@ -0,0 +1,12 @@
+package simple_util
+
+import "os"
+
+// check if a file exists an is not a directory
+func FileExists(fileName string) bool {
+	info, err := os.Stat(fileName)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
