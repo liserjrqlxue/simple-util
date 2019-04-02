d0e8ef3932c5c8652ec0dda74dd3746e83d9ef83
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-04-02 13:41:20 +0800

    add LongFiles2MapArray

--- txt.go
+++ txt.go
@@ -76,6 +76,30 @@ func Files2MapArray(fileNames []string, sep string, skip *regexp.Regexp) (Data [
 	return
 }
 
+// read files to []map[string]string
+func LongFiles2MapArray(fileNames []string, sep string, skip *regexp.Regexp) (Data []map[string]string, Title []string) {
+	for _, fileName := range fileNames {
+		data, title := LongFile2MapArray(fileName, sep, skip)
+		for _, item := range data {
+			Data = append(Data, item)
+		}
+		if len(Title) == 0 {
+			Title = title
+		} else {
+			if len(Title) != len(title) {
+				log.Fatal("titles has different columns")
+			} else {
+				for i := 0; i < len(Title); i++ {
+					if Title[i] != title[i] {
+						log.Fatal("titles not equal")
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
 // read file to map[string]map[string]string
 func File2MapMap(fileName, key, sep string) map[string]map[string]string {
 	file, err := os.Open(fileName)
