1c52ccdfb13d9c146e2a0a980308e50e84922e1e
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-07-11 14:53:20 +0800

    add CopyFile

--- file.go
+++ file.go
@@ -1,6 +1,10 @@
 package simple_util
 
-import "os"
+import (
+	"io"
+	"log"
+	"os"
+)
 
 // check if a file exists an is not a directory
 func FileExists(fileName string) bool {
@@ -9,4 +13,25 @@ func FileExists(fileName string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+func CopyFile(dst, src string) (err error) {
+	r, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer DeferClose(r)
+
+	w, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer DeferClose(w)
+
+	n, err := io.Copy(w, r)
+	if err != nil {
+		return
+	}
+	log.Printf("CopyFile %d bytes[%s -> %s]", n, src, dst)
+	return w.Sync()
 }
